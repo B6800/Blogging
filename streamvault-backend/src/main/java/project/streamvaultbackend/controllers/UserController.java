1581afb0b9ddd66f5a3130240ff1c99c1bf98e48
B6800 <bryanbakwowi@gmail.com>
2025-06-21 22:26:53 +0200

    -Annotated this class with @Transcational to prevent LazyInitializationExeption -Added a constructor for  userService -Modified the URL path for searchUsers,follow,unfollow methods -Added getFollowers method with it's corresponding URL path -Removed private method getCurrentUser

---
+++ streamvault-backend/src/main/java/project/streamvaultbackend/controllers/UserController.java
@@ -0,0 +1,52 @@
+package project.streamvaultbackend.controllers;
+
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import project.streamvaultbackend.dtos.UserDto;
+import project.streamvaultbackend.entities.User;
+import project.streamvaultbackend.services.UserService;
+
+import java.util.List;
+@CrossOrigin(origins = "*")
+@RestController
+@Transactional
+@RequestMapping("/api/users")
+public class UserController {
+
+   private final UserService userService;
+
+    @Autowired
+    UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/search")//commit
+    public List<UserDto> searchUsers(@RequestParam String username,@RequestParam String query) {
+        return userService.searchUsers(query, username);
+    }
+    @PostMapping("/{id}/follow")
+    public void follow(@PathVariable Long id,@RequestParam String username) {
+        User currentUser = userService.findByUsername(username);
+        userService.follow(currentUser, id);
+    }
+
+    @PostMapping("/{id}/unfollow")
+    public void unfollow(@PathVariable Long id,@RequestParam String username) {
+        User currentUser = userService.findByUsername(username);
+        userService.unfollow(currentUser, id);
+    }
+    @GetMapping("/{id}/followers")
+    public List<UserDto> getFollowers(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return user.getFollowers().stream()
+                .map(follower -> new UserDto(
+                        follower.getId(),
+                        follower.getUsername(),
+                        follower.getAvatar(),
+                        false // or: user.getFollowing().contains(follower)
+                ))
+                .toList();
+    }
+
+}
