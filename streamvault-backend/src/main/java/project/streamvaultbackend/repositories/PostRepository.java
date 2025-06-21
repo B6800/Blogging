3c4b5ed52338c7e39717b9a7b4e6e76f11702f36
B6800 <bryanbakwowi@gmail.com>
2025-06-21 23:15:51 +0200

    - Replaced empty class with correct interface extending JpaRepository - Added methods for querying posts by user and user IDs with pagination

--- streamvault-backend/src/main/java/project/streamvaultbackend/repositories/PostRepository.java
+++ streamvault-backend/src/main/java/project/streamvaultbackend/repositories/PostRepository.java
@@ -1,5 +1,13 @@
 package project.streamvaultbackend.repositories;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import project.streamvaultbackend.entities.Post;
 import project.streamvaultbackend.entities.User;
-public class PostRepository {
+
+import java.util.List;
+import java.util.Set;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByUserOrderByTimestampDesc(User user, PageRequest pageable);
+    List<Post> findByUserIdInOrderByTimestampDesc(Set<Long> userIds, PageRequest pageable);
 }
