05bb0092b201458a63e54cb154356d0797cbf770
B6800 <bryanbakwowi@gmail.com>
2025-06-26 21:13:22 +0200

    Added ownedByCurrentUser field to PostDto for ownership checks on posts

--- streamvault-backend/src/main/java/project/streamvaultbackend/dtos/PostDto.java
+++ streamvault-backend/src/main/java/project/streamvaultbackend/dtos/PostDto.java
@@ -1,4 +1,4 @@
 package project.streamvaultbackend.dtos;
 
-public record PostDto(long id,String username,String avatar,String text,long timestamp,int likeCount,boolean likedByCurrentUser) {
+public record PostDto(long id,String username,String avatar,String text,long timestamp,int likeCount,boolean likedByCurrentUser,boolean ownedByCurrentUser ) {
 }
