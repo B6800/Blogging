f2e6efc42757e508085681ecaac7cc1e1d41880e
B6800 <bryanbakwowi@gmail.com>
2025-07-02 20:22:30 +0200

    Comment entity

---
+++ streamvault-backend/src/main/java/project/streamvaultbackend/entities/Comment.java
@@ -0,0 +1,48 @@
+package project.streamvaultbackend.entities;
+
+import jakarta.persistence.*;
+
+@SuppressWarnings("ALL")
+@Entity
+public class Comment {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false)
+    private Post post;
+
+    @ManyToOne(optional = false)
+    private User user;
+
+    @Column(nullable = false, length = 280)
+    private String text;
+
+    private long timestamp;
+
+    // Getters & setters
+    public Long getId() {
+        return id;
+    }
+    public Post getPost() {
+        return post;
+    }
+    public void setPost(Post post) {
+        this.post = post;
+    }
+    public User getUser() {
+        return user; }
+    public void setUser(User user) {
+        this.user = user;
+    }
+    public String getText() {
+        return text; }
+    public void setText(String text) {
+        this.text = text;
+    }
+    public long getTimestamp() {
+        return timestamp;
+    }
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
