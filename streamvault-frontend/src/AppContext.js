efd5aa61d70a9254d0da761e44b24eac92c0d0a0
B6800 <bryanbakwowi@gmail.com>
2025-07-06 20:59:21 +0200

    -Set AppContext default to VITE_APP_STREAMVAULT_API value

--- streamvault-frontend/src/AppContext.js
+++ streamvault-frontend/src/AppContext.js
@@ -1,4 +1,4 @@
 // src/AppContext.js
 import React from "react";
-const AppContext = React.createContext(undefined);
+const AppContext = React.createContext(import.meta.env.VITE_APP_STREAMVAULT_API);
 export default AppContext;
